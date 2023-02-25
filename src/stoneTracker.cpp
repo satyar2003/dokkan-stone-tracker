06462bf2314f8c6c2e6ea0bb3c325c272c474113
satyar2003 <satyar2003@gmail.com>
2023-02-24 21:08:01 -0800

    Added menu to do actions

--- src/stoneTracker.cpp
+++ src/stoneTracker.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include "../header/weekend.h"
+#include <vector>
 #include "../header/table.h"
 
 using namespace std;
@@ -10,14 +10,44 @@ int main() {
     cout << "Enter the number of dragon stones you currently have: ";
     cin >> currentStones;
     cout << "Enter the number of days you want to save: ";
-    cin >> days;
-
+    cin >> days;    
     Weekend w = Weekend(days);
     totalStones += currentStones + 2*days + w.getWeekendStones() + w.stonesFromExtraDays();
 
-    Table t = Table();
-    cout << endl;
-    t.outputTable(currentStones, days, w);
+    int choice = 1;
+    vector<Events> eventList;
+    while (choice != 0) {
+        cout << endl;
+        cout << "(1) Add an event" << endl;
+        cout << "(2) Output total amount of stones" << endl;
+        cout << "(3) Exit the program" << endl;
+        cout << "Choose an option: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                {
+                    Events e = Events();
+                    if (e.getEventType() != invalid) {
+                        eventList.push_back(e);
+                    }
+                    break;
+                }
+            case 2:
+                {
+                    Table t = Table();
+                    cout << endl;
+                    t.outputTable(currentStones, days, w);
+                    break;
+                }
+            case 3:
+                choice = 0;
+                break;
+            default:
+                cout << "Invalid option" << endl;
+                break;
+        }
+    }
 
     return 0;
 }
