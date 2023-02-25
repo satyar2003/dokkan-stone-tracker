95dd942ffd07a35d57ad848aa9eba3760ac0f401
satyar2003 <satyar2003@gmail.com>
2023-02-24 20:11:51 -0800

    Update table output

--- src/table.cpp
+++ src/table.cpp
@@ -11,7 +11,9 @@ Table::~Table() {}
 void Table::outputTable(int currentStones, int days, Weekend w) {
     cout << left << setw(20) << "Current Stones" << currentStones << endl;
     cout << left << setw(20) << "Weekend Stones" << w.getWeekendStones() + w.stonesFromExtraDays() << endl;
+    cout << "---------------------------" << endl;
     cout << left << setw(20) << "Login Bonus" << days << endl;
     cout << left << setw(20) << "Special Missions" << days << endl;
+    cout << "---------------------------" << endl;
     cout << left << setw(20) << "Total Stones" << currentStones + w.getWeekendStones() + w.stonesFromExtraDays() + days*2 << endl;
 }
