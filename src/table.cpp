479c6513a9cac72bdd7fb08295d267df788bd9cd
satyar2003 <satyar2003@gmail.com>
2023-02-25 00:07:43 -0800

    Added more outputs for table

--- src/table.cpp
+++ src/table.cpp
@@ -34,5 +34,96 @@ void Table::outputTable(int currentStones, int days, Weekend w, vector<Events> e
         }
     }
 
+    for (int i = 0; i < e.size(); ++i) {
+        if (e.at(i).getEventType() == dbStory) {
+            cout << left << setw(20) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
+            totalStones += e.at(i).getStoneCount();
+        }
+    }
+    for (int i = 0; i < e.size(); ++i) {
+        if (e.at(i).getEventType() == dbStory) {
+            cout << "---------------------------" << endl;
+            break;
+        }
+    }
+
+    for (int i = 0; i < e.size(); ++i) {
+        if (e.at(i).getEventType() == strikeEvent) {
+            cout << left << setw(20) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
+            totalStones += e.at(i).getStoneCount();
+        }
+    }
+    for (int i = 0; i < e.size(); ++i) {
+        if (e.at(i).getEventType() == strikeEvent) {
+            cout << "---------------------------" << endl;
+            break;
+        }
+    }
+
+    for (int i = 0; i < e.size(); ++i) {
+        if (e.at(i).getEventType() == dokkanEvent) {
+            cout << left << setw(20) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
+            totalStones += e.at(i).getStoneCount();
+        }
+    }
+    for (int i = 0; i < e.size(); ++i) {
+        if (e.at(i).getEventType() == dokkanEvent) {
+            cout << "---------------------------" << endl;
+            break;
+        }
+    }
+
+    for (int i = 0; i < e.size(); ++i) {
+        if (e.at(i).getEventType() == ultimateClash) {
+            cout << left << setw(20) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
+            totalStones += e.at(i).getStoneCount();
+        }
+    }
+    for (int i = 0; i < e.size(); ++i) {
+        if (e.at(i).getEventType() == ultimateClash) {
+            cout << "---------------------------" << endl;
+            break;
+        }
+    }
+
+    for (int i = 0; i < e.size(); ++i) {
+        if (e.at(i).getEventType() == primeBattle) {
+            cout << left << setw(20) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
+            totalStones += e.at(i).getStoneCount();
+        }
+    }
+    for (int i = 0; i < e.size(); ++i) {
+        if (e.at(i).getEventType() == primeBattle) {
+            cout << "---------------------------" << endl;
+            break;
+        }
+    }
+
+    for (int i = 0; i < e.size(); ++i) {
+        if (e.at(i).getEventType() == EZA) {
+            cout << left << setw(20) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
+            totalStones += e.at(i).getStoneCount();
+        }
+    }
+    for (int i = 0; i < e.size(); ++i) {
+        if (e.at(i).getEventType() == EZA) {
+            cout << "---------------------------" << endl;
+            break;
+        }
+    }
+
+    for (int i = 0; i < e.size(); ++i) {
+        if (e.at(i).getEventType() == other) {
+            cout << left << setw(20) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
+            totalStones += e.at(i).getStoneCount();
+        }
+    }
+    for (int i = 0; i < e.size(); ++i) {
+        if (e.at(i).getEventType() == other) {
+            cout << "---------------------------" << endl;
+            break;
+        }
+    }
+
     cout << left << setw(20) << "Total Stones" << totalStones << endl;
 }
