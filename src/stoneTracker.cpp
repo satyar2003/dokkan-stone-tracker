c1ac328d91aacc725a76994436bbdfa719374126
satyar2003 <satyar2003@gmail.com>
2023-02-27 14:40:10 -0800

    Updated logic for completeEvent()

--- src/stoneTracker.cpp
+++ src/stoneTracker.cpp
@@ -125,7 +125,20 @@ void completeEvent(vector<Events> &e) {
     cin >> option;
     bool b = true;
     while(b) {
-        if(cin.good() && (option >= 1 || option <= e.size())) b = false;
+        if(cin.good()){
+            if (option >= 1 || option <= e.size()) b = false;
+            else {
+                cin.clear();
+                cout << "Invalid input" << endl;
+                cin.ignore(numeric_limits<streamsize>::max(),'\n');
+                for (int i = 0; i < e.size() - 1; ++i) {
+                    cout << "(" << i + 1 << ") Mark " << e.at(i).getEventName() << " as completed" << endl;
+                }
+                cout << "(" << e.size() << ") Mark " << e.at(e.size()-1).getEventName() << " as completed";
+                cout << "Choose an option: ";
+                cin >> option;
+            }
+        } 
         else {
             cin.clear();
             cout << "Invalid input" << endl;
