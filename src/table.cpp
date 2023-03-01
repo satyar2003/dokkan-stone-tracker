595c30b5620db08cb665ed21db681870b20bd699
satyar2003 <satyar2003@gmail.com>
2023-02-28 18:48:57 -0800

    Made code more readable

--- src/table.cpp
+++ src/table.cpp
@@ -8,15 +8,17 @@ Table::Table() {}
 
 Table::~Table() {}
 
-// Prints dividing lines 10 characters longer than inputted int
+// Prints dividing line 10 characters longer than inputted int [based on columnWidth variable from outputTable()]
 void printDivider(int);
+// Prints all events of a certain even types with corresponding stone count 
+void printEventType(int, vector<Events>, eventType, int &);
 
 // Prints table with total stones divided by category
 void Table::outputTable(int currentStones, int days, Weekend w, vector<Events> e) {
     int totalStones = 0;
     int columnWidth = 30;
     for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventName().size() + 1 > columnWidth) {
+        if (e.at(i).getEventName().size() + 5 > columnWidth) {
             columnWidth = e.at(i).getEventName().size() + 5;
         }
     }
@@ -25,116 +27,20 @@ void Table::outputTable(int currentStones, int days, Weekend w, vector<Events> e
     cout << left << setw(columnWidth) << "Weekend Stones" << w.getWeekendStones() + w.stonesFromExtraDays() << endl;
     totalStones += currentStones + w.getWeekendStones() + w.stonesFromExtraDays();
     printDivider(columnWidth);
-    cout << endl;
 
     cout << left << setw(columnWidth) << "Login Bonus" << days << endl;
     cout << left << setw(columnWidth) << "Special Missions" << days << endl;
     totalStones += days*2;
     printDivider(columnWidth);
 
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == storyEvent) {
-            cout << left << setw(columnWidth) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
-            totalStones += e.at(i).getStoneCount();
-        }
-    }
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == storyEvent) {
-            printDivider(columnWidth);
-            break;
-        }
-    }
-
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == dbStory) {
-            cout << left << setw(columnWidth) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
-            totalStones += e.at(i).getStoneCount();
-        }
-    }
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == dbStory) {
-            printDivider(columnWidth);
-            break;
-        }
-    }
-
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == strikeEvent) {
-            cout << left << setw(columnWidth) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
-            totalStones += e.at(i).getStoneCount();
-        }
-    }
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == strikeEvent) {
-            printDivider(columnWidth);
-            break;
-        }
-    }
-
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == dokkanEvent) {
-            cout << left << setw(columnWidth) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
-            totalStones += e.at(i).getStoneCount();
-        }
-    }
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == dokkanEvent) {
-            printDivider(columnWidth);
-            break;
-        }
-    }
-
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == ultimateClash) {
-            cout << left << setw(columnWidth) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
-            totalStones += e.at(i).getStoneCount();
-        }
-    }
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == ultimateClash) {
-            printDivider(columnWidth);
-            break;
-        }
-    }
-
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == primeBattle) {
-            cout << left << setw(columnWidth) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
-            totalStones += e.at(i).getStoneCount();
-        }
-    }
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == primeBattle) {
-            printDivider(columnWidth);
-            break;
-        }
-    }
-
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == EZA) {
-            cout << left << setw(columnWidth) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
-            totalStones += e.at(i).getStoneCount();
-        }
-    }
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == EZA) {
-            printDivider(columnWidth);
-            break;
-        }
-    }
-
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == other) {
-            cout << left << setw(columnWidth) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
-            totalStones += e.at(i).getStoneCount();
-        }
-    }
-    for (int i = 0; i < e.size(); ++i) {
-        if (e.at(i).getEventType() == other) {
-            printDivider(columnWidth);
-            break;
-        }
-    }
+    printEventType(columnWidth, e, storyEvent, totalStones);
+    printEventType(columnWidth, e, dbStory, totalStones);
+    printEventType(columnWidth, e, strikeEvent, totalStones);
+    printEventType(columnWidth, e, dokkanEvent, totalStones);
+    printEventType(columnWidth, e, ultimateClash, totalStones);
+    printEventType(columnWidth, e, primeBattle, totalStones);
+    printEventType(columnWidth, e, EZA, totalStones);
+    printEventType(columnWidth, e, other, totalStones);
 
     cout << left << setw(columnWidth) << "Total Stones" << totalStones << endl;
 }
@@ -144,4 +50,16 @@ void printDivider(int x) {
         cout << "-";
     }
     cout << endl;
+}
+
+void printEventType(int x, vector<Events> e, eventType et, int &total) {
+    bool b = false;
+    for (int i = 0; i < e.size(); ++i) {
+        if (e.at(i).getEventType() == et) {
+            cout << left << setw(x) << e.at(i).getEventName() << e.at(i).getStoneCount() << endl;
+            total += e.at(i).getStoneCount();
+            b = true;
+        }
+    }
+    if (b) printDivider(x);
 }
